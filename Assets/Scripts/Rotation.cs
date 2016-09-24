b388a4c41809360246200dcda164b414b5f82e8f
AudiologMusic <fuad@vonloops.com>
2016-09-24 16:29:06 +0200

    added more detail

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    //==============================================================================
+    // Fields
+    //==============================================================================
+
+    private float m_Speed = 4f;
+    private float m_Rotation = 0;
+
+    //==============================================================================
+    // MonoBehaviour
+    //==============================================================================
+    void FixedUpdate()
+    {
+        m_Rotation += m_Speed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, m_Rotation, transform.rotation.eulerAngles.z);
+    }
+}
