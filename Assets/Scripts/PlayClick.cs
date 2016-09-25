bfa3fd012fcededd3c52dfd5c0e471cdc2c332f0
Ulf R. Pedersen <ulf@urp.dk>
2016-09-25 08:51:18 +0200

    Added click  sound

---
+++ Assets/Scripts/PlayClick.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayClick : MonoBehaviour {
+
+    //==============================================================================
+    // Fields
+    //==============================================================================
+    [SerializeField]
+    private AudioSource m_clickSound;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Debug.Log("Pressed left click.");
+            m_clickSound.Play();
+        }
+    }
+}
