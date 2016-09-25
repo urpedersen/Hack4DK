0287bc866f932f983a1e86cb71037a13c3e1404e
Ulf R. Pedersen <ulf@urp.dk>
2016-09-25 10:59:04 +0200

    Mover again ...

--- Assets/Scripts/GazeMover.cs
+++ Assets/Scripts/GazeMover.cs
@@ -22,6 +22,7 @@ public class GazeMover : MonoBehaviour
 
     [SerializeField] private TextMesh m_text;
 
+    private int m_StepCounter = 0;
 
     //==============================================================================
     // MonoBehaviours
@@ -59,19 +60,16 @@ public class GazeMover : MonoBehaviour
         if (m_isMoving)
         {
             Vector3 desiredPosition = m_moveTo.transform.position;
-            float distance = Vector3.Distance(desiredPosition, m_Camera.transform.position);
-            if (distance>0.01) // desiredPosition != m_Camera.transform.position
+            //float distance = Vector3.Distance(desiredPosition, m_Camera.transform.position);
+            if (m_StepCounter < 100000) // desiredPosition != m_Camera.transform.position
             {
-                m_Camera.transform.position = Vector3.MoveTowards(m_Camera.transform.position, desiredPosition, distance*m_speed);
-                //stupidCounter++;
-                /*if (stupidCounter>30)
-                {
-                    m_isMoving = false;
-                    stupidCounter = 0;
-                    desiredPosition = m_Camera.transform.position;
-                }*/
+                //m_Camera.transform.position = Vector3.MoveTowards(m_Camera.transform.position, desiredPosition, distance*m_speed);
+                //m_Camera.transform.Translate((desiredPosition-m_Camera.transform.position) * Time.deltaTime  );
+                m_StepCounter++;
+
             } else {
                 m_isMoving = false;
+                m_StepCounter = 0;
             }
         }
 
---
+++ Assets/Scripts/GazePlaySound.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazePlaySound : MonoBehaviour {
+
+    //==============================================================================
+    // Fields
+    //==============================================================================
+    private GazeBase m_GazeBase;
+
+    [SerializeField] private AudioSource m_Sound;
+
+    // Use this for initialization
+    void Start () {
+        m_GazeBase = GetComponent<GazeBase>();
+        m_GazeBase.onGazeBeginCallBack += OnGazeStart;
+        m_GazeBase.onGazeEndCallBack += OnGazeEnd;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+
+    //==============================================================================
+    // Public
+    //==============================================================================
+
+    //==============================================================================
+    public void OnGazeStart()
+    {
+        if (m_Sound != null)
+            m_Sound.Play();
+    }
+
+    //==============================================================================
+    public void OnGazeEnd()
+    {
+
+    }
+}
